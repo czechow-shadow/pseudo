67793df6200c3d89b21b2550fd33c5b2def1a6b2
czechow <pawel.czechowicz@vp.pl>
2018-06-01 08:27:05 +0200

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (projectile-tags-command . "find src app -type f | grep hs$ | xargs hasktags -e")))
