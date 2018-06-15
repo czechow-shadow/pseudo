9d3c22323d5cbbf9c26abb7cd0229e8b2cdc61c1
pc84828 <pawel.czechowicz@citi.com>
2018-06-15 19:43:28 +0200

    dev

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,6 +23,7 @@ import           GHC.IO.Exception         (IOErrorType (HardwareFault))
 -- import qualified Data.Text as T
 -- import qualified Data.ByteString as BS
 
+import qualified Data.Text                as T
 import           Network.Socket
 
 
@@ -47,12 +48,13 @@ fork_exec_with_pty = do
 
 
 go :: IO ()
-go = do
-  putText "Listening"
-  void $ timeout (1000 * 1000 * 5) $ bracket openSocket closeSocket acceptLoop
-  putText "Sleeping"
-  threadDelay $ 1000 * 1000 * 5
-  go
+go = go3
+-- go = do
+--   putText "Listening"
+--   void $ timeout (1000 * 1000 * 5) $ bracket openSocket closeSocket acceptLoop
+--   putText "Sleeping"
+--   threadDelay $ 1000 * 1000 * 5
+--   go
 
 
 openSocket :: IO Socket
@@ -67,6 +69,7 @@ openSocket = do
 closeSocket :: Socket -> IO ()
 closeSocket sock = close sock
 
+-- FIXME: res leak if killed between accept and async...
 acceptLoop :: Socket -> IO ()
 acceptLoop sock = forever $ do
   (sock', peer) <- accept sock
@@ -77,15 +80,16 @@ talk :: Socket -> IO ()
 talk sock = do
   putText $ "Spawning bash and talking to it"
   bracket allocPty closePty $ \(fd, ph) ->
-    withAsync (sockToPty sock fd) $ \as1 ->
+    withAsync (sockToPty sock fd) $ \as1 -> do
     withAsync (ptyToSock sock fd) $ \as2 -> do
-    void $ waitAny [as1, as2]
-    void $ waitForProcess ph
+      void $ waitAny [as1, as2]
+      void $ waitForProcess ph
   putText $ "Talk finished"
   where
     allocPty = fork_exec_with_pty
     closePty = closeFd . fst
 
+-- FIXME: socket close will cause Exception, I think
 sockToPty :: Socket -> Fd -> IO ()
 sockToPty sock fd =
   allocaArray bs $ \buf -> sockToPty' buf
@@ -98,6 +102,89 @@ sockToPty sock fd =
           pure ()
         _ -> do _ <- fdWriteBuf fd buf (fromIntegral c) -- FIXME: check write cnt
                 sockToPty' buf
+
+type LeftOvers = Text -- ByteString
+
+data ParsePhase = PPIn | PPOut deriving Show
+
+
+data CryptCtx = CryptCtx
+data ParseCtx = ParseCtx ParsePhase LeftOvers deriving Show
+
+empytParseCtx :: ParseCtx
+empytParseCtx = ParseCtx PPOut ""
+
+data Message = Message Text deriving Show
+type ErrText = Text
+
+
+-- go3 :: IO ()
+-- go3 = runStateT go3' (ParseCtx PPOut "") >>= putText . show
+
+go3 :: IO ()
+go3 = go' empytParseCtx
+  where
+    go' pctx = receive0 >>= \case
+      Just xs -> do let (res, pctx') = receive' xs pctx
+                    putText $ show res
+                    go' pctx'
+      Nothing -> putText "Eof"
+
+
+go3' :: (MonadIO m, MonadState ParseCtx m) => m [Either ErrText Message]
+go3' = receive0 >>= \case
+  Just xs -> (++) <$> receiveM xs <*> go3'
+  Nothing -> pure []
+
+
+receive0 :: (MonadIO m) => m (Maybe Text)
+receive0 = do
+  putText "get line> "
+  liftIO $ try (liftIO getLine) >>= \case
+    Right x -> pure $ Just x
+    Left (_::IOException) -> pure Nothing
+
+receiveM :: (MonadState ParseCtx m) => Text -> m [Either ErrText Message]
+receiveM xs = do
+  ctx <- get
+  let (res, ctx') = receive' xs ctx
+  put ctx'
+  pure res
+
+receive' :: Text -> ParseCtx -> ([Either ErrText Message], ParseCtx)
+receive' buf pctx@(ParseCtx _ leftOvers)
+ | T.length buf + T.length leftOvers > 8192 =
+   ([Left "Message too long, contents dropped"], ParseCtx PPOut "")
+ | otherwise = receive'' buf pctx
+
+receive'' :: Text -> ParseCtx -> ([Either ErrText Message], ParseCtx)
+receive'' buf (ParseCtx pp lo) = case pp of
+  PPOut -> case T.span (/= '<') (lo <> buf) of
+    ("", ys) -> receive'' ys (ParseCtx PPIn "")
+    (xs, "") -> ([Left $ garbage xs], ParseCtx pp "")
+    (xs, ys) -> first ((Left $ garbage xs):) $
+                      receive'' ys (ParseCtx PPIn "")
+  PPIn -> case T.span (/= '>') (lo <> buf) of
+    (xs, "") -> ([], ParseCtx pp xs)
+    (xs, ys) -> first ((Right $ Message $ xs <> T.take 1 ys):) $
+                      receive'' (T.drop 1 ys) (ParseCtx PPOut "")
+  where
+    garbage xs = "Dropped garbage: [" <> xs <> "]"
+
+
+
+
+recvMessage :: Socket -> IO ()
+recvMessage = undefined
+
+-- do we rely on exceptions?
+recvEncrypted :: Socket -> Ptr Word8 -> StateT (CryptCtx, ParseCtx) IO ()
+recvEncrypted sock buf = do
+  (cCtx, pCtx) <- get
+  c <- liftIO $ recvBuf sock buf (fromIntegral bs) -- FIXME: handle exception
+
+  undefined
+
 
 ptyToSock :: Socket -> Fd -> IO ()
 ptyToSock sock fd =
